984786d40bce30cb6757da286ad25640558b9b65
harshshrivastava15 <harshshrivastava.work@gmail.com>
2023-11-18 15:46:58 +0530

    edited Practiacl  class

--- practicals/Practical.php
+++ practicals/Practical.php
@@ -1,58 +1,50 @@
 <?php
 
-/**
- * Class Practical
- *
- * Contains static functions for practical tasks.
- */
-class Practical
-{
+class Practical {
     /**
      * Prints "Hello, World!"
      */
-    public static function printHello()
-    {
+    public static function printHello() {
         echo "Hello, World!";
     }
 
     /**
      * Adds two numbers and returns the sum.
-     *
-     * @param int|float $num1 The first number.
-     * @param int|float $num2 The second number.
-     *
-     * @return int|float The sum of the two numbers.
+     * @param int $num1 The first number to be added.
+     * @param int $num2 The second number to be added.
+     * @return int The sum of $num1 and $num2.
      */
-    public static function add($num1, $num2)
-    {
+    public static function add($num1, $num2) {
+        if(!is_numeric($num1) || !is_numeric($num2))
+        {
+            throw new \InvalidArgumentException("Arguments must be numeric");
+        }
         return $num1 + $num2;
     }
 
     /**
      * Generates a Fibonacci sequence containing 'n' integers.
-     *
-     * @param int $n The number of elements in the Fibonacci sequence.
-     *
-     * @return array The Fibonacci sequence as an array.
+     * @param int $n The number of Fibonacci numbers to generate.
+     * @return array An array containing the first 'n' Fibonacci numbers.
      */
-    public static function generateFibonacciSequence($n)
-    {
-        $sequence = [];
-
+    public static function generateFibonacciSequence($n) {
+        if(!is_int($n))
+        {
+            throw new \InvalidArgumentException("Arguments must be Integer");
+        }
+        $fibonacciSequence = array();
         if ($n >= 1) {
-            $sequence[] = 0;
+            $fibonacciSequence[] = 0;
         }
-
         if ($n >= 2) {
-            $sequence[] = 1;
-            for ($i = 2; $i < $n; $i++) {
-                // Each subsequent number is the sum of the two preceding ones
-                $sequence[] = $sequence[$i - 1] + $sequence[$i - 2];
-            }
+            $fibonacciSequence[] = 1;
         }
-
-        return $sequence;
+        for ($i = 2; $i < $n; $i++) {
+            $fibonacciSequence[] = $fibonacciSequence[$i - 1] + $fibonacciSequence[$i - 2];
+        }
+        return $fibonacciSequence;
     }
 }
 
 ?>
+
