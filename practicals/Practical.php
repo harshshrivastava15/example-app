e91321fcff19a9615eb0e38155f3c3bee4e48b8f
harshshrivastava15 <harshshrivastava.work@gmail.com>
2023-11-18 15:55:04 +0530

    edited Practical class and practicaltest

--- practicals/Practical.php
+++ practicals/Practical.php
@@ -1,50 +1,50 @@
 <?php
+namespace practicals;
 
 class Practical {
-    /**
-     * Prints "Hello, World!"
-     */
-    public static function printHello() {
+    // Static function to print "Hello, World!"
+    public static function printHello()
+    {
         echo "Hello, World!";
     }
 
-    /**
-     * Adds two numbers and returns the sum.
-     * @param int $num1 The first number to be added.
-     * @param int $num2 The second number to be added.
-     * @return int The sum of $num1 and $num2.
-     */
+    // Static function to add two numbers
     public static function add($num1, $num2) {
-        if(!is_numeric($num1) || !is_numeric($num2))
-        {
-            throw new \InvalidArgumentException("Arguments must be numeric");
+        // Convert input values to numeric type if they are numeric strings
+        if (is_numeric($num1)) {
+            $num1 = is_string($num1) ? (float) $num1 : $num1;
+        } else {
+            throw new \InvalidArgumentException("Input value 1 must be numeric.");
         }
+
+        if (is_numeric($num2)) {
+            $num2 = is_string($num2) ? (float) $num2 : $num2;
+        } else {
+            throw new \InvalidArgumentException("Input value 2 must be numeric.");
+        }
+
         return $num1 + $num2;
     }
 
-    /**
-     * Generates a Fibonacci sequence containing 'n' integers.
-     * @param int $n The number of Fibonacci numbers to generate.
-     * @return array An array containing the first 'n' Fibonacci numbers.
-     */
+    // Static function to generate a Fibonacci sequence
     public static function generateFibonacciSequence($n) {
-        if(!is_int($n))
-        {
-            throw new \InvalidArgumentException("Arguments must be Integer");
+        if (!is_int($n)) {
+            throw new \InvalidArgumentException("Input value must be an integer.");
         }
-        $fibonacciSequence = array();
-        if ($n >= 1) {
-            $fibonacciSequence[] = 0;
-        }
-        if ($n >= 2) {
-            $fibonacciSequence[] = 1;
-        }
-        for ($i = 2; $i < $n; $i++) {
-            $fibonacciSequence[] = $fibonacciSequence[$i - 1] + $fibonacciSequence[$i - 2];
+
+        $fibonacciSequence = [];
+        if ($n <= 0) {
+            return $fibonacciSequence; // Return an empty array for non-positive integers.
+        } elseif ($n == 1) {
+            $fibonacciSequence[] = 0; // Special case for n = 1
+        } else {
+            $fibonacciSequence = [0, 1]; // Initialize the sequence with the first two numbers
+            for ($i = 2; $i < $n; $i++) {
+                $fibonacciSequence[] = $fibonacciSequence[$i - 1] + $fibonacciSequence[$i - 2];
+            }
         }
         return $fibonacciSequence;
     }
 }
-
 ?>
 
--- tests/Unit/PracticalTest.php
+++ tests/Unit/PracticalTest.php
@@ -14,7 +14,24 @@ class PracticalTest extends TestCase
      */
     public function testAdd(): void
     {
+        $this->assertEquals(Practical::add(1.5,3), 4.5);
         $this->assertEquals(Practical::add(1,3), 4);
+        $this->assertEquals(Practical::add("1.5","3"), 4.5);
+    }
+
+    public function testAddNumeric(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        Practical::add("abc","xyz");
+    }
+
+    public function testFibonacci(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        Practical::generateFibonacciSequence(5.5);
+        $this->expectException(\InvalidArgumentException::class);
+        Practical::generateFibonacciSequence("5.5");
+        //$this->assertEquals(Practical::generateFibonacciSequence("5"),);
     }
 }
 
