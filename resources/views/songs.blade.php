b84d796f14efc3802ab84654dc73d407f942a986
harshshrivastava15 <harshshrivastava.work@gmail.com>
2023-11-25 14:27:29 +0530

    making  HTML static page to dynamic display songs

--- resources/views/songs.blade.php
+++ resources/views/songs.blade.php
@@ -1,26 +1,3 @@
-<!DOCTYPE html>
+<x-layouts.main pageName='All Songs'>
 
-<head>
-
-    <title>Songs Page</title>
-</head>
-<body>
-    <h1>Hello Everyone</h1>
-    <h2>Songs Class</h2>
-    <table border="" >
-     @foreach($songs as $song)
-    <tr>
-        <th>Title: </th>
-        <th>Artist: </th>
-        
-    </tr>
-    <tr>
-        <td>{{ $song -> title }}</td>
-        <td>{{ $song -> artist }}</td>
-    </tr>
-    @endforeach
-    </table>
-
-
-</body>
-</html>
+</x-layouts.main>
